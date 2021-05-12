3cd3ac7ea72c5f73e3106fee761cd9f0e85ebe26
kuspia <63403330+kuspia@users.noreply.github.com>
2021-05-12 17:16:12 +0530

    Update app.js

--- static/app.js
+++ static/app.js
@@ -37,7 +37,7 @@ function onClickedEstimatePrice() {
 //  var url = "/api/predict_home_price"; 
   // var url = "https://blrhomeprice.herokuapp.com/predict_home_price"
   
-   var url = "https://blrhomeprice.herokuapp.com:5000/predict_home_price"
+   var url = "https://blrhomeprice.herokuapp.com:443/predict_home_price"
   
   $.post(url, {
       total_sqft: parseFloat(sqft.value),
@@ -61,7 +61,7 @@ function onPageLoad() {
  // var url = "https://blrhomeprice.herokuapp.com/"+":"+port+"get_location_names"
  // var url = "https://blrhomeprice.herokuapp.com/"+":"+port+"/get_location_names"
  
- var url = "https://blrhomeprice.herokuapp.com:5000/get_location_names"
+ var url = "https://blrhomeprice.herokuapp.com:443/get_location_names"
   $.get(url,function(data, status) {
       console.log("got response for get_location_names request");
       if(data) {
