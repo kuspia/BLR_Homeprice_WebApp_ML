9f616405ade5fcde7f593f759abd6379e9ad1ffa
kuspia <63403330+kuspia@users.noreply.github.com>
2021-05-13 16:45:58 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,54 @@
 #Author-Kushagra Shukla
 from flask import Flask, request, jsonify , render_template
 import util
-import os
+import pickle
+import json
+import numpy as np
+
+
+locations = None
+data_columns = None
+model = None
+
+def get_estimated_price(location,sqft,bhk,bath):
+    try:
+        loc_index = data_columns.index(location.lower())
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index>=0:
+        x[loc_index] = 1
+
+    return round(model.predict([x])[0],2)
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global data_columns #will be used as a list
+    global locations #will be used as a list
+
+    with open("home.json", "r") as f:
+        data_columns = json.load(f)['data_columns']
+        locations = data_columns[3:]  # first 3 columns are sqft, bath, bhk so we ignore them and take only the locations
+
+    global model
+    if model is None:
+        with open('home.pickle', 'rb') as f:
+            model = pickle.load(f)
+    print("loading saved artifacts...done")
+
+def get_location():
+    return locations
+
+def get_data_columns():
+    return data_columns
+
+
+
+
 
 app = Flask(__name__)
 @app.route('/')
@@ -12,10 +59,10 @@ def home():
 @app.route('/get_location_names', methods=['GET']) #Routine to get location names
 def get_location_names():
     response = jsonify({
-        'locations': util.get_location_names()
+        'locations': get_location()
     })
+    print(locations)
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
 @app.route('/predict_home_price', methods=['GET', 'POST'])
@@ -26,7 +73,7 @@ def predict_home_price():
     bath = int(request.form['bath'])
 
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+        'estimated_price': get_estimated_price(location,total_sqft,bhk,bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
@@ -34,5 +81,5 @@ def predict_home_price():
 
 if __name__ == "__main__":
     print("Starting Python Flask For Home Price Prediction...")
-    util.load_saved_artifacts()
-    app.run( port=49486)
+    load_saved_artifacts()
+    app.run(port=5000)
